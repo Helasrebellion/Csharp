1120c3795be00683ed071914ad3417b780b7d62d
Sylvia Hela Mullins <49255017+Helasrebellion@users.noreply.github.com>
2022-08-01 20:31:18 -0400

    Create input2.cs

---
+++ input2.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace input2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            //take the name as input
+            string name = Console.ReadLine();
+            
+            
+            Console.WriteLine("Hello {0}. Welcome to our event",name);
+        }
+    }
+}
