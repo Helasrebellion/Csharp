4c3ea0f4935f0c6fdb0083fd08efc8a64ce2d966
Sylvia Hela Mullins <49255017+Helasrebellion@users.noreply.github.com>
2022-08-01 21:07:18 -0400

    Create yearlySalary.cs

---
+++ yearlySalary.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class yearlySalary
+    {
+        static void Main(string[] args)
+        {
+          //your code goes here
+          decimal monthSalary =
+          int.Parse(Console.ReadLine());
+          Console.WriteLine(monthSalary * 12);
+
+        }
+    }
+}
