c7d177877072b52e2a46df70cd4c58039129eb0d
Sylvia Hela Mullins <49255017+Helasrebellion@users.noreply.github.com>
2022-08-02 17:09:15 -0400

    Create whileLoop.cs

---
+++ whileLoop.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace whileLoop
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = 3;
+            while (number >= 0)
+			{
+				Console.WriteLine(number);
+				number--;
+			}
+		}
+	}
+}
